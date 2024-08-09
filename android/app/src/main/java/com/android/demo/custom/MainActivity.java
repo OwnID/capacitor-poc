df326311dd942e32d602b7b9691b6809e221d99a
Dmytro Kryvoruchko <dkrivoruchko@gmail.com>
2024-08-09 14:52:06 +0300

    Update to Android OwnID Core 3.4.0-beta06

---
+++ android/app/src/main/java/com/android/demo/custom/MainActivity.java
@@ -0,0 +1,22 @@
+package com.android.demo.custom;
+
+import android.os.Bundle;
+
+import com.getcapacitor.BridgeActivity;
+import com.ownid.sdk.OwnId;
+import com.ownid.sdk.OwnIdInstanceKt;
+import com.ownid.sdk.OwnIdWebViewBridge;
+
+import java.util.Collections;
+import java.util.HashSet;
+
+public class MainActivity extends BridgeActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        OwnIdWebViewBridge webViewBridge = OwnIdInstanceKt.createWebViewBridge(OwnId.getInstance(), null, null);
+        webViewBridge.injectInto(bridge.getWebView(), new HashSet<>(Collections.singletonList("https://dev.ownid.com")), this, true, null);
+    }
+}
